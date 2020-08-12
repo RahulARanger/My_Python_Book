fe5d1926422cf8750c8418af23f3fe1f862a4dec
Rahul <saihanumarahul66@gmail.com>
2020-08-12 16:07:54 +0530

    Some Special Methods and Special Constants are added in the Special_Methods/ and Special_constants respectively

---
+++ Special_methods/all()_any().py
@@ -0,0 +1,7 @@
+a=[True,False,True]
+print(any(a))
+print(all(a))
+a=[True,True]
+print(any(a))
+print(all(a))
+# ! all() and any() are only used on iterable objects
---
+++ Special_methods/chr()_ascii()_ord().py
@@ -0,0 +1,11 @@
+# chr() converts the ascii value to the unicode string
+a=(1,2)
+print(ascii(a))
+a=[1,2] 
+print(ascii(a))
+a={1:2}
+print(ascii(a))
+print(chr(97))
+# ! ascii() is opposite of the eval() it returns the string format of any object
+# * ord() converts the unicode to ascii value
+print(ord('a'))
