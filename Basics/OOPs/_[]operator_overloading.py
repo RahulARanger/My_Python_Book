494aa86b057c3b18d324390e89c0b45daa3849d7
Rahul <saihanumarahul66@gmail.com>
2020-09-03 15:07:31 +0530

    a text file containg link for more predefined functions and variables is added in Basics/OOPs/

--- Basics/OOPs/_[]operator_overloading.py
+++ Basics/OOPs/_[]operator_overloading.py
@@ -7,9 +7,13 @@ class test:
         self.lst[key]=value
     def __str__(self):
         return str(self.lst)
+    def __delitem__(self,key):
+        del self.lst[key]
 a=test()
 print(a)
 a[6]=6
 # we can even slice it 
 print(a[3:6])
+print(a)
+del a[3]
 print(a)
