37187d199df30818e9df8e131dadb384be395557
Rahul <saihanumarahul66@gmail.com>
2020-09-03 11:28:34 +0530

    Binary Search is modified in Algorithms/Searching/

--- DataScience/Data Structures and Algorithms/Algorithms/Searching/Binary_search_O(logn).py
+++ DataScience/Data Structures and Algorithms/Algorithms/Searching/Binary_search_O(logn).py
@@ -1,22 +1,14 @@
-def bs(lst,k,start,end):
-    print(start,end)
-    index=(start+end)//2
-    if start>end:return False,len(lst)-1
-    if start==end-1:
-        if k==lst[start]:return True,start
-        elif k==lst[end]:return True,end
-        else:return False,start
-    if lst[index]==k:return True,index
-    elif lst[index]>k:return bs(lst,k,start,index-1)
-    elif lst[index]<k:return bs(lst,k,index+1,end)
-lst=[int(i) for i in input('Enter Some Numbers: ').split()]
+def bs(lst,key,start=0,end=None):
+    if end is None:end=len(lst)-1
+    if start>end:return -1
+    mid=(start+end)//2
+    if lst[mid]==key:return mid
+    else:
+        return bs(lst,key,start,mid-1) if lst[mid]>key else bs(lst,key,mid+1,end)
+lst=[int(i) for i in input('Enter Some Numbers (in ascending order): ').split()]
 lst.sort()
 k=int(input('Enter a Number to search: '))
-res,ans=bs(lst,k,0,len(lst)-1)
-if res:
-    print('The Element is present at the Index: ',ans+1)
-else:
-    print('The given element is not present in the list\n')
-    print('We can insert the new element at the index: ',ans+1)    
-
-    # The Time Complexity of the Binary Algorithm is O(log(n))
+ans=bs(lst,k)
+if ans!=-1: print('Element is Found at: {}'.format(ans))
+else: print('Not Present')
+# ?  The Time Complexity of the Binary Algorithm is O(log(n))
