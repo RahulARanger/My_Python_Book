d0918cb9e0e25d0d77342f50031441f6d23fad05
Rahul <saihanumarahul66@gmail.com>
2020-09-18 21:04:47 +0530

    GUI folder is now cleanly arranged to certain extend

--- GUI/Kiwi/readme.txt
+++
@@ -1,2 +0,0 @@
-in all these files it seems there would be some underlines under kivy ignore them
-works when executed through python name_of _the_file.py
--- GUI/Tkinter/grid_positioning.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-root=Tk()
-label1=Label(root,text='Hello World!!!')
-label2=Label(root,text='Thank You!!!')
-label1.grid(row=0,column=0)
-label2.grid(row=1,column=0)
-root.mainloop()
