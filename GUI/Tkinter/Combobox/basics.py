d0d62d6818bab16ba6cccf6a40881bd69b1935f9
Rahul <saihanumarahul66@gmail.com>
2020-09-26 16:23:28 +0530

    Combobox is expained in GUI/Tkinter

---
+++ GUI/Tkinter/Combobox/basics.py
@@ -0,0 +1,7 @@
+from tkinter import *
+import tkinter.ttk as ttk
+root=Tk()
+ae=['a','fsadgfsdgsdfgdsgsdfsfdsfdsfsdfsdsdsadsadsadsadasddfdsfsdfdsf','c']
+a=ttk.Combobox(root,values=ae)
+a.pack()
+root.mainloop()
