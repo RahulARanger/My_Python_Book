8051f9db0feece2b6c1913ca77c90d4681f3cacd
Rahul <saihanumarahul66@gmail.com>
2020-09-30 20:44:09 +0530

    git commit -m 'Binary search file is updated in algorithms'

--- DataScience/Data Structures and Algorithms/Algorithms/Searching/Binary_search_O(logn).py
+++ DataScience/Data Structures and Algorithms/Algorithms/Searching/Binary_search_O(logn).py
@@ -1,14 +1,12 @@
-def bs(lst,key,start=0,end=None):
-    if end is None:end=len(lst)-1
-    if start>end:return -1
-    mid=(start+end)//2
-    if lst[mid]==key:return mid
-    else:
-        return bs(lst,key,start,mid-1) if lst[mid]>key else bs(lst,key,mid+1,end)
-lst=[int(i) for i in input('Enter Some Numbers (in ascending order): ').split()]
-lst.sort()
-k=int(input('Enter a Number to search: '))
-ans=bs(lst,k)
-if ans!=-1: print('Element is Found at: {}'.format(ans))
-else: print('Not Present')
-# ?  The Time Complexity of the Binary Algorithm is O(log(n))
+def bs(lst,s,e,x):
+    mid=(s+e)//2
+    if lst[mid]==x:return mid
+    if s>e:return -1
+    if lst[mid]>x:return bs(lst,s,mid-1,x)
+    else:return bs(lst,mid+1,e,x)
+if __name__=='__main__':
+    a=[int(i) for i in input('Enter the List of Numbers: ').split()]
+    a.sort()
+    note=(bs(a,0,len(a)-1,int(input('Enter the element to search: '))))
+    if note==-1:print('NOT_FOUND')
+    else:print('Found at:',note+1)
