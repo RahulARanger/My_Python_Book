492d932f6bff91fb8e331503bf2e1bc333324ec3
Rahul <saihanumarahul66@gmail.com>
2020-12-07 17:37:06 +0530

    * Started with Socket Programming in Python

--- Interaction with the OS/test.py
+++
@@ -1,8 +0,0 @@
-import re
-results=['Jan 31 00:09:39 ubuntu.local ticky: ERROR Permission denied while closing ticket (ac)','Jan 31 00:09:39 ubuntu.local ticky: INFO Created ticket [#4217] (mdouglas)']
-users=[]
-for i in results:
-    if 'ERROR' in i:
-        print(re.search(r'ERROR ([^(]+)',i)[1])
-    users.append(re.search(r'\(([^ ]+)\)',i)[1])
-print(users)    
