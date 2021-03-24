7d22d5341991172240a6f42d3238cfdeeb4cc1e2
RahulARanger <saihanumarahul66@gmail.com>
2021-03-24 11:31:50 +0530

    Cleaned Unnecessary Files again

--- Numpy/Arrays/subclass/actual_method.py
+++
@@ -1,9 +0,0 @@
-import numpy
-
-class test(numpy.ndarray):
-    def __new__(self, *args, **kwargs):
-        pass
-    
-    
-    def __init__(self, container, dtype, *args, **kwargs):
-        pass
